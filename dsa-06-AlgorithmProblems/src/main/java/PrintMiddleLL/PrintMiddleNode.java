1242d76c9cd06656b506e26fc8413c4cb76998af
Osman MERT <osman_mrt@yahoo.com>
2023-09-29 14:50:50 +0300

    Initial commit

---
+++ dsa-06-AlgorithmProblems/src/main/java/PrintMiddleLL/PrintMiddleNode.java
@@ -0,0 +1,27 @@
+package PrintMiddleLL;
+
+public class PrintMiddleNode {
+    public static void main(String[] args) {
+        MySinglyLinkedList sll=new MySinglyLinkedList();
+        for (int i = 1; i <= 6; i++) {
+            sll.add(i);
+        }
+        printMiddle(sll);
+    }
+    public static void printMiddle(MySinglyLinkedList sll){
+        Node a = sll.head;
+        Node b = sll.head;
+
+        while (b!=sll.tail && b.next!=sll.tail){
+            a = a.next;
+            b = b.next.next;
+        }
+
+        if (b==sll.tail){
+            System.out.println("Middle Number: "+a.id);
+        } else
+            System.out.println("Middle Numbers: "+a.id+" "+ b.id);
+
+    }
+
+}
